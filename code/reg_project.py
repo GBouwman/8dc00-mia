128500be05f2eb9bdb95bbb9c4f5f33f4da24751
GBouwman <glennbouwman@gmail.com>
2019-09-19 12:33:33 +0200

    Start of project

---
+++ code/reg_project.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import registration as reg
+from IPython.display import display, clear_output
+
+
+def intensity_based_registration(I, Im, Affine = True):
+
+    # initial values for the parameters
+    # we start with the identity transformation
+    # most likely you will not have to change these
+    if Affine:
+        x = np.array([0.,0.,0.,0.,0.,1.,1.])
+        fun = lambda x: reg.affine_corr(I, Im, x)
+    else:
+        x = np.array([0., 0., 0.])
+        fun = lambda x: reg.rigid_corr(I, Im, x)
+
+    # the learning rate
+    mu = 0.001
+
+    # number of iterations
+    num_iter = 200
+
+    iterations = np.arange(1, num_iter+1)
+    similarity = np.full((num_iter, 1), np.nan)
+
+    fig = plt.figure(figsize=(14,6))
+
+    # fixed and moving image, and parameters
+    ax1 = fig.add_subplot(121)
+
+    # fixed image
+    im1 = ax1.imshow(I)
+    # moving image
+    im2 = ax1.imshow(I, alpha=0.7)
+    # parameters
+    txt = ax1.text(0.3, 0.95,
+        np.array2string(x, precision=5, floatmode='fixed'),
+        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
+        transform=ax1.transAxes)
+
+    # 'learning' curve
+    ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
+
+    learning_curve, = ax2.plot(iterations, similarity, lw=2)
+    ax2.set_xlabel('Iteration')
+    ax2.set_ylabel('Similarity')
+    ax2.grid()
+
+    # perform 'num_iter' gradient ascent updates
+    for k in np.arange(num_iter):
+
+        # gradient ascent
+        g = reg.ngradient(fun, x)
+        x += g*mu
+
+        # for visualization of the result
+        S, Im_t, _ = fun(x)
+
+        clear_output(wait = True)
+
+        # update moving image and parameters
+        im2.set_data(Im_t)
+        txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
+
+        # update 'learning' curve
+        similarity[k] = S
+        learning_curve.set_ydata(similarity)
+
+        display(fig)
